f2c66010b0baac99fd4d7b8dabc2368da1a35642
deco-jpg <srdaniloh@gmail.com>
2025-03-13 20:51:00 -0300

    Update 10.atividade_combustivel.py

--- Atividades_Pontuadas/10.atividade_combustivel.py
+++ Atividades_Pontuadas/10.atividade_combustivel.py
@@ -1,5 +1,8 @@
 import os
 os.system ("clear")
+#Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado da seguinte
+#forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que
+#O preço do litro da gasolina é R$ 6,59 e o preço do litro do álcool é R$ 3,79.
 
 litros = float (input("Escola a quantidade de litros: "))
 tipo_combustivel = input ("Escolha o tipo de combustivel (A- Alcool | (G- Gasolina: ") .strip() .upper()
